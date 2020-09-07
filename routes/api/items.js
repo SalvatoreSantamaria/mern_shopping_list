04f39a3574479e0ccdfba7cb9eb0d768b508f3eb
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-07 10:25:43 -0700

    Created Post and Delete API

--- routes/api/items.js
+++ routes/api/items.js
@@ -5,12 +5,32 @@ const router = express.Router();
 const Item = require('../../models/Item');
 
 // @route GET api/items
-// @desc Get All items
+// @desc Get All Items
 // @access Public
 router.get('/', (req, res) => {
   Item.find()
     .sort({date: -1 })
     .then(items => res.json(items))
 });
+
+// @route Post api/items
+// @desc Create Item
+// @access Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item))
+});
+
+// @route Delete api/items/:id
+// @desc Create Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  deleteItem = Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}));
+});
+
 
 module.exports = router;
