a169a9d049ba31f1332d1ecea0488736e663961c
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-07 20:56:48 -0700

    Added nav bar

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppNavbar from './components/AppNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar></AppNavbar>
+    </div>
+  );
+}
+
+export default App;
+
+// start client and server with npm run dev
