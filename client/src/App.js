93e451eece56efc7b2d2e1d7ee72b00176f23569
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-08 06:27:35 -0700

    Revert "Added nav bar"

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import AppNavbar from './components/AppNavbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AppNavbar></AppNavbar>
+      { 'Hello'
+      /* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
     </div>
   );
 }
