eed9d45d65ec86d299e831ff84f09e9abe6ce859
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-19 10:41:43 -0700

    Added 'X' button for UI removal

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -31,6 +31,16 @@ class ShoppingList extends Component {
             {items.map(({ id, name }) => (
               <CSSTransition key={id} timeout={500} classNames="fade">
                 <ListGroupItem>
+                  <Button
+                  className="remove-btn"
+                  color="danger"
+                  size="sm"
+                  onClick={()=> { 
+                    this.setState(state => ({
+                      items: state.items.filter(item => item.id !== id)
+                    }));
+                  }}
+                  >&times;</Button>
                   {name}
                 </ListGroupItem>
               </CSSTransition>
