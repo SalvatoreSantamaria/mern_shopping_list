8fd7616941abc4b472ff6351fe4ff91e640042f8
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-19 19:41:21 -0700

    Finish Redux

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -17,6 +17,18 @@ export default function(state = initialState, action) {
       return { 
         ...state
       }
+      case DELETE_ITEM: 
+        return {
+          ...state,
+          items: state.items.filter(item => item.id !== action.payload)
+        }
+
+        case ADD_ITEM: 
+        return {
+          ...state,
+          items: [action.payload, ...state.items]
+        };
+
     default: 
       return state;
   }
